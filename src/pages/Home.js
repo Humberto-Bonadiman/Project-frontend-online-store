b368488fb4fed5f7f3602077f17f2ed0d9eabebe
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-20 17:57:24 -0400

    Correçao de erro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ItensCards from '../components/ItensCards';
 import { getProductsFromCategoryAndQuery } from '../services/api';
-import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   constructor() {
