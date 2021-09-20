b8e6738809ae7c33d083f10113838fe499d1cc0f
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-20 17:47:59 -0400

    Requisito 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,67 @@
 import React from 'react';
+import ItensCards from '../components/ItensCards';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      search: '',
+      apiResults: [],
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      search: event.target.value,
+    });
+  }
+
+  async handleClick() {
+    const { search } = this.state;
+    const categoryAndQuery = await getProductsFromCategoryAndQuery('', search);
+    const resultsCategoryAndQuery = categoryAndQuery.results;
+    this.setState({
+      apiResults: resultsCategoryAndQuery,
+    });
+  }
+
   render() {
+    const { search, apiResults } = this.state;
     return (
       <div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <form>
+          <label htmlFor="search">
+            Buscar:
+            <input
+              type="text"
+              data-testid="query-input"
+              name="search"
+              value={ search }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            data-testid="query-button"
+          >
+            Procurar
+          </button>
+        </form>
+        <div>
+          { apiResults.map((product) => (<ItensCards
+            key={ product.id }
+            product={ product }
+          />)) }
+        </div>
       </div>
     );
   }
