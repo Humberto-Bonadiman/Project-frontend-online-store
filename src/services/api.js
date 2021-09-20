2a7a5f06407f4ea72ded2b13b3cd5dfefb869769
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-20 19:40:57 +0100

    Ajuste lint requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export async function getCategories() {
   return dataJSON;
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const data = await fetch(URL);
   const dataJSON = await data.json();
